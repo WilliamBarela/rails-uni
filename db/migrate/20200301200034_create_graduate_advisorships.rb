e6bf2e2c52a4ee74d8ec69afd1dca49e54b649cc
William Barela <william.barela.dev@gmail.com>
2020-03-01 15:22:08 -0600

    add comments to graduate_advisorship for database

--- db/migrate/20200301200034_create_graduate_advisorships.rb
+++ db/migrate/20200301200034_create_graduate_advisorships.rb
@@ -1,11 +1,11 @@
 class CreateGraduateAdvisorships < ActiveRecord::Migration[6.0]
   def change
-    create_table :graduate_advisorships do |t|
-      t.boolean :primary_advisor?
-      t.date :advising_initiation
-      t.date :advising_termination
-      t.belongs_to :faculty_member, index: true, null: false, foreign_key: true
-      t.belongs_to :graduate_student, index: true, null: false, foreign_key: true
+    create_table :graduate_advisorships, comment: "join table of faculty_members and graduate_students" do |t|
+      t.boolean :primary_advisor?, comment: "true, false"
+      t.date :advising_initiation, comment: "when advisory relationship commenced"
+      t.date :advising_termination, comment: "when advisory relationship ended"
+      t.belongs_to :faculty_member, index: true, null: false, foreign_key: true, comment: "faculty members may have many advisees"
+      t.belongs_to :graduate_student, index: true, null: false, foreign_key: true, comment: "graduate students may have many advisors"
 
       t.timestamps
     end
