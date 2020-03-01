029ab0a797548b178d9f441a3822d864666d5764
William Barela <william.barela.dev@gmail.com>
2020-03-01 15:09:06 -0600

    add comments to graduate_students migration for database

--- db/migrate/20200301194410_create_graduate_students.rb
+++ db/migrate/20200301194410_create_graduate_students.rb
@@ -1,17 +1,17 @@
 class CreateGraduateStudents < ActiveRecord::Migration[6.0]
   def change
-    create_table :graduate_students do |t|
-      t.string :current_program
-      t.string :degree_sought
-      t.string :auxiliary_roles
-      t.integer :expected_graduation_year
-      t.string :expected_graduation_semester
-      t.text :objectives_statement
-      t.text :research_interests_summary
-      t.date :role_start_date
-      t.date :role_termination_date
-      t.string :termination_reason
-      t.references :person, index: true, null: false, foreign_key: true
+    create_table :graduate_students, comment: "GraduateStudents table for all graduate student roles" do |t|
+      t.string :current_program, comment: "Biology, Microbiology, ..."
+      t.string :degree_sought, comment: "PhD, Master, PSM, ..."
+      t.string :auxiliary_roles, comment: "GA, RA, TA, ..."
+      t.integer :expected_graduation_year, comment: "2020, 2021, ..."
+      t.string :expected_graduation_semester, comment: "Spring, Fall, Summer"
+      t.text :objectives_statement, comment: "160 or less character long objectives/mission statement"
+      t.text :research_interests_summary, comment: "paragraph(s) stating summary of research interests"
+      t.date :role_start_date, comment: "date in which graduate student's current_program role started"
+      t.date :role_termination_date, comment: "date in which graduate student's currrent_program role finished"
+      t.string :termination_reason, comment: "default: null; contract ended, fired, quit, ..."
+      t.references :person, index: true, null: false, foreign_key: true, comment: "one person to possibly many graduate student roles"
 
       t.timestamps
     end
