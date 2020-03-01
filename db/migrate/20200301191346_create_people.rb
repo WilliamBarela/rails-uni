e148ca858f1a2ba0113fc3ee51752349a08f1284
William Barela <william.barela.dev@gmail.com>
2020-03-01 14:19:58 -0600

    add comments to people migration for database

--- db/migrate/20200301191346_create_people.rb
+++ db/migrate/20200301191346_create_people.rb
@@ -1,16 +1,16 @@
 class CreatePeople < ActiveRecord::Migration[6.0]
   def change
-    create_table :people do |t|
-      t.string :honorific
-      t.string :last_name
-      t.string :middle_name
-      t.string :first_name
-      t.string :suffix
-      t.string :gender
-      t.string :ttus_email
-      t.string :ttus_phone
-      t.date :ttus_termination_date
-      t.string :termination_reason
+    create_table :people, comment: "People Table: info for all members of the department (excluding role ino)" do |t|
+      t.string :honorific, comment: "person honorific: Mr., Miss, Ms., Mrs., Dr., ..."
+      t.string :last_name, comment: "person surname"
+      t.string :middle_name, comment: "person middle name (optional)"
+      t.string :first_name, comment: "person given name"
+      t.string :suffix, comment: "person suffix: I, II, Jr., Sr., ..."
+      t.string :gender, comment: "person gender: female, male, non-binary"
+      t.string :ttus_email, comment: "person TTU email username@ttu.edu, username@ttuhsc.edu"
+      t.string :ttus_phone, comment: "person work phone"
+      t.date :ttus_termination_date, comment: "date of dissociation from TTU"
+      t.string :termination_reason, comment: "reason for termination from TTU"
 
       t.timestamps
     end
