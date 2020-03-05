7ff0e41d1da11219a84505adf2781a61cfef8a04
William Barela <william.barela.dev@gmail.com>
2020-03-04 22:54:51 -0600

    add Person seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Faker::Config.locale = 'en-US'
+
+20.times { 
+  Person.new(
+    honorific: ['Mr.', 'Mrs.', 'Ms.', 'Dr.'].sample,
+    last_name: Faker::Name.unique.last_name,
+    middle_name: Faker::Name.unique.name,
+    first_name: Faker::Name.unique.first_name,
+    suffix: ['Jr.', 'II', 'III', 'IV'].tap{|a| 100.times{ a << nil }}.sample,
+    gender: ['male', 'female', 'non-binary'].sample,
+    ttus_email: Faker::Internet.email,
+    ttus_phone: "806-834-#{Faker::PhoneNumber.subscriber_number(length: 4)}"
+  ).save
+}
