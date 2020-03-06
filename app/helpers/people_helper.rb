4ad53aba05f59fd146bea2e0392e5fd6e7fbc9b3
William Barela <william.barela.dev@gmail.com>
2020-03-06 00:32:46 -0600

    refactor display_name to people helper

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,2 +1,6 @@
 module PeopleHelper
+  def display_name(person)
+    name = "#{person.honorific} #{person.first_name} #{person.last_name}"
+    (person.suffix.nil?) ? name : "#{name}, #{person.suffix}"
+  end
 end
