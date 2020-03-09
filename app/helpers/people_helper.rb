7d2a213521dcc46ec379d71872640b1123968085
William Barela <william.barela.dev@gmail.com>
2020-03-08 22:42:05 -0500

    add helper method to display current degree program

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -3,4 +3,8 @@ module PeopleHelper
     name = "#{person.honorific} #{person.first_name} #{person.last_name}"
     (person.suffix.nil?) ? name : "#{name}, #{person.suffix}"
   end
+
+  def display_degree_program(grad_student)
+    "#{grad_student.degree_sought} in #{grad_student.current_program}"
+  end
 end
